1bff3056d5048788df7393553d490ecb2d2f7a80
Jace Harmer <jaceharmer@gmail.com>
2017-01-26 23:29:13 -0800

    Let's try adding some markup.

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,4 +1,13 @@
-import Html
+import Html exposing (div, h1, h2, section, text)
+import Html.Attributes exposing (class)
 
 
-main = Html.text "Hello world"
+main =
+  div [class "app-wrapper"]
+    [ section []
+      [ h1 [] [text "Hello World"]
+      ]
+    , section []
+      [ h2 [] [text "This is really important."]
+      ]
+    ]
